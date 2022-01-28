b6e77a03cc44908833e4e694cf078d10e1d2128d
Matheus Konrath <matheus.konrath@compasso.com.br>
2022-01-28 16:01:55 -0300

    Update category

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -9,14 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dscatalog.dto.CategoryDto;
-import com.devsuperior.dscatalog.entities.Category;
-import com.devsuperior.dscatalog.repository.CategoryRepository;
 import com.devsuperior.dscatalog.services.CategoryService;
 
 @RestController
@@ -48,4 +47,15 @@ public class CategoryResource {
 		return ResponseEntity.created(uri).body(dto);
 		
 	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CategoryDto> update(@PathVariable Long id, @RequestBody CategoryDto dto){
+		dto = categoryService.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+		
+		
+	}
+	
+	
+	
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,15 +4,13 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-
-import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import com.devsuperior.dscatalog.dto.CategoryDto;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repository.CategoryRepository;
@@ -33,18 +31,30 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDto findById(Long id) {
 		Optional<Category> obj = categoryRepository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new CategoryDto(entity);
 	}
 
+	@Transactional
 	public CategoryDto insert(CategoryDto dto) {
 		Category entity = new Category();
 		entity.setName(dto.getName());
 		entity = categoryRepository.save(entity);
 		return new CategoryDto(entity);
 	}
-	
-	
-	
+
+	@Transactional
+	public CategoryDto update(Long id, CategoryDto dto) {
+		try {
+		Category entity = categoryRepository.getOne(id);
+		entity.setName(dto.getName());
+		entity = categoryRepository.save(entity);
+		return new CategoryDto(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		}
+		
+	}	
 
 }
