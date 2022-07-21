942e3f161090e1839c636ce1535c0d59249a27a4
Matheus Konrath <matheus.konrath@compasso.com.br>
2022-07-21 16:47:16 -0300

    adjust Dascatalog Unit Tests

--- backend/src/test/java/com/devsuperior/dscatalog/DscatalogApplicationTest.java
+++ backend/src/test/java/com/devsuperior/dscatalog/DscatalogApplicationTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DscatalogApplicationTests {
+public class DscatalogApplicationTest {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads(){
+    }
+    
 }
