7e88c8be9614d249d83b978f7a3ca6b39c54c641
Matheus Konrath <mkonrath96@gmail.com>
2023-01-27 18:35:55 -0300

    adjust find all page

--- backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTests.java
@@ -5,13 +5,13 @@ import com.devsuperior.dscatalog.services.ProductService;
 import com.devsuperior.dscatalog.tests.Factory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.List;
 
@@ -19,35 +19,33 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 
-@WebMvcTest(ProductResourceTests.class)
+@WebMvcTest(ProductResource.class)
 public class ProductResourceTests {
-
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
-    private ProductService service;
+    private ProductService productService;
 
     private PageImpl<ProductDto> page;
-    private ProductDto productDto;
+    private ProductDto productDTO;
 
     @BeforeEach
-    void setUp() {
-        productDto = Factory.createProductDto();
-        page = new PageImpl<>(List.of(productDto));
-
-        Mockito.when(service.findAllPaged(any())).thenReturn(page);
+    void setUp() throws Exception {
+        productDTO = Factory.createProductDto();
+        page = new PageImpl<>(List.of(productDTO));
+        when(productService.findAllPaged(any())).thenReturn(page);
 
     }
 
     @Test
-    public void findAllShouldReturnPage() throws Exception{
-        
-      mockMvc.perform(get("/products")).andReturn();
+    public void findAllPageShouldReturnPafe() throws Exception {
+        ResultActions result = mockMvc.perform(get("/products")
+                .accept(MediaType.APPLICATION_JSON));
 
+        result.andExpect(status().isOk());
     }
 
 }
